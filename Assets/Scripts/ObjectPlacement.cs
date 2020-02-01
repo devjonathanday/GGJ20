c4c1d094254714dbbbccbe46d511464c19a16fa7
Jonathan <devjonathanday@gmail.com>
2020-01-31 22:22:25 -0800

    Added object placement script (WIP)

---
+++ Assets/Scripts/ObjectPlacement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPlacement : MonoBehaviour
+{
+    [Header("Object Properties")]
+    public Transform myObject;
+    public float objectHeight;
+    public Transform cam;
+    public LayerMask placeableLayers;
+    public float raycastDistance;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if(Physics.Raycast(cam.position, cam.forward, out RaycastHit hit, raycastDistance, placeableLayers))
+        {
+            myObject.position = new Vector3(Mathf.RoundToInt(hit.point.x), hit.point.y + (objectHeight / 2), Mathf.RoundToInt(hit.point.z));
+        }
+    }
+}
