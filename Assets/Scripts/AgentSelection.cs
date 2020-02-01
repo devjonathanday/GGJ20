ac0a0df998049aa7cbd33ab899f9b17bbfd28d85
Jonathan <devjonathanday@gmail.com>
2020-02-01 11:07:20 -0800

    Added new layers, yeet

---
+++ Assets/Scripts/AgentSelection.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AgentSelection : MonoBehaviour
+{
+    public List<AgentNavigation> selectedAgents = new List<AgentNavigation>();
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+}
