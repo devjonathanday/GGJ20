e0dad3ba18b15eb90287f19e656310fab9a934c0
Paul Gibbons <31017045+QuestGiver@users.noreply.github.com>
2020-01-31 22:47:42 -0800

    Add money machine and players pocket(the money hole)

---
+++ Assets/Scripts/Machines/MoneyMachine.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoneyMachine : MonoBehaviour
+{
+    public float Health = 10;
+    public float Income = 10;
+    public float IncomeTickRate = 1;
+
+    float timer = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer > IncomeTickRate)
+        {
+            PlayerPocket.Money += Income;
+            timer = 0;
+        }
+    }
+}
---
+++ Assets/Scripts/UICode/PlayerPocket.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPocket : MonoBehaviour
+{
+
+    private static float money;
+
+    public static float Money
+    {
+        get => money;
+        set
+        {
+            if (money != value)
+            {
+                money = value;
+                Debug.Log(money);
+            }
+
+        }
+    }
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        Money = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
