23bf3a545e2ba58d423889814020a1c86408041e
Paul Gibbons <31017045+QuestGiver@users.noreply.github.com>
2020-02-01 17:39:21 -0800

    Added meteor spawner

---
+++ Assets/Scripts/CIAMeteorMissiles/MeteorTargeting.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeteorTargeting : MonoBehaviour
+{
+    public float SpawnHight = 50;
+    public GameObject Meteor;
+    public Vector2 FieldSize;
+    public float SpawnRate = 10;
+    float timer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        timer += Time.deltaTime;
+        if (timer >= SpawnRate)
+        {
+            Instantiate(Meteor,
+                new Vector3(
+                            Random.Range(-FieldSize.x/2, FieldSize.x/2),
+                            SpawnHight,
+                            Random.Range(-FieldSize.y / 2, FieldSize.y / 2)
+                           ),
+                        this.transform.rotation);
+            timer = 0;
+        }
+    }
+}
--- Assets/Scripts/Navigation/AgentNavigation.cs
+++ Assets/Scripts/Navigation/AgentNavigation.cs
@@ -25,7 +25,7 @@ public class AgentNavigation : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+       
     }
 
     public void GoToDestination()
