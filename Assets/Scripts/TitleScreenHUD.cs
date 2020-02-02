da2b8051a5e2fda4bc873f54cdd9b285f794825b
Jonathan <devjonathanday@gmail.com>
2020-02-02 12:09:20 -0800

    Title screen and music

---
+++ Assets/Scripts/TitleScreenHUD.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleScreenHUD : MonoBehaviour
+{
+    public GameObject titleMenu;
+    public GameObject controlsMenu;
+    public string gameSceneName;
+
+    public void DisplayControls()
+    {
+        controlsMenu.SetActive(true);
+        titleMenu.SetActive(false);
+    }
+    public void StartGame()
+    {
+        SceneManager.LoadScene(gameSceneName);
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
